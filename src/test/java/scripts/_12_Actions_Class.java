1056966475f211075c4b70cc5e80bfc025be801a
akim92270 <akim.92270@gmail.com>
2022-06-11 14:35:19 -0500

    added more actions

--- src/test/java/scripts/_12_Actions_Class.java
+++ src/test/java/scripts/_12_Actions_Class.java
@@ -2,20 +2,18 @@ package scripts;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.GoogleSearchPage;
 import utilities.Waiter;
 
-import java.security.Key;
 import java.time.Duration;
 import java.util.List;
 
-public class _12_Actions_Class extends Base{
+public class _12_Actions_Class extends Base {
     @Test(priority = 1, description = "Actions | move to element 1")
-    public void moveToElementPractice1(){
+    public void moveToElementPractice1() {
         driver.get("https://www.techglobalschool.com/");
 
         WebElement address = driver.findElement(By.cssSelector("#comp-kixny3ro span>span"));
@@ -27,7 +25,7 @@ public class _12_Actions_Class extends Base{
     }
 
     @Test(priority = 2, description = "Actions class | move to element 2")
-    public void moveToElementPractice2(){
+    public void moveToElementPractice2() {
         driver.get("https://www.carvana.com/");
 
         /*WebElement financeDropdown = null;
@@ -62,7 +60,7 @@ public class _12_Actions_Class extends Base{
      */
 
     @Test(priority = 3, description = "Actions | Click")
-    public void clickOnElementPractice(){
+    public void clickOnElementPractice() {
         driver.get("https://www.techglobalschool.com/");
 
         WebElement registerForIntroLink = driver.findElement(By.cssSelector("#comp-kvjohd5t5>a"));
@@ -73,6 +71,94 @@ public class _12_Actions_Class extends Base{
         actions.moveToElement(registerForIntroLink).pause(Duration.ofSeconds(2)).click().perform();
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.techglobalschool.com/apply-now-1");
-
-
     }
+
+    /*
+    Go to https://www.techglobalschool.com/
+    Right click on "Register for Intro Session" link
+    Validate the url is https://www.techglobalschool.com/
+     */
+
+    @Test(priority = 4, description = "Actions | Right Click")
+    public void rightClickOnElementPractice() {
+        driver.get("https://www.techglobalschool.com/");
+        WebElement registerForIntroLink = driver.findElement(By.cssSelector("#comp-kvjohd5t5>a"));
+
+        actions.contextClick(registerForIntroLink).perform();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.techglobalschool.com/");
+    }
+
+    /*PRACTICE
+
+    Go to https://demoqa.com/droppable
+    Click and hold ”Drag me” box and release it on “Drop here” box
+    Validate “Drop here” text is replaced with “Dropped!”
+
+    Method name = clickHoldAndReleasePractice()
+    Test name = Actions class | click-hold-release an element*/
+
+
+    @Test(priority = 5, description = "Actions | Click - hold and release | Drag and drop")
+    public void clickHoldAndReleasePractice() {
+        driver.get("https://demoqa.com/droppable");
+
+        WebElement dragMeBox = driver.findElement(By.id("draggable"));
+        WebElement dropHereBox = driver.findElement(By.id("droppable"));
+
+        //actions.clickAndHold(dragMeBox).moveToElement(dropHereBox).release().perform();
+        actions.dragAndDrop(dragMeBox, dropHereBox).perform();
+
+        Assert.assertEquals(dropHereBox.getText(), "Dropped!");
+    }
+
+    /*
+    Go to https://www.google.com/
+    Enter "Hello" to search input box
+    Validate value attribute is "Hello"
+
+    Method name = sendKeysToAnElement ()
+    Test name = Actions class | send keys to an element
+     */
+    @Test(priority = 6, description = "Actions | Send Keys to an element")
+    public void sendKeysToAnElement() {
+        driver.get("https://www.google.com/");
+        Waiter.pause(10);
+        actions.sendKeys(driver.findElement(By.name("q")), "Hello").perform();
+        Waiter.pause(10);
+        Assert.assertEquals(driver.findElement(By.name("q")).getAttribute("value"), "Hello");
+    }
+
+
+    /*
+Go to https://www.google.com/
+Enter "techglobal" to search input box with uppercases
+Validate the value attribute for search input box is "TECHGLOBAL"
+Then, copy the text and paste again
+Validate the value attribute for search input box is "TECHGLOBALTECHGLOBAL"
+ */
+
+    @Test(priority = 7, description = "Actions | key down and key up")
+    public void practiceKeyDownAndKeyUp(){
+        driver.get("https://www.google.com/");
+
+        //googleSearchPage.searchInputBox.sendKeys("TECHGLOBALTECHGLOBAL");
+
+        actions.keyDown(Keys.SHIFT)
+                .sendKeys(googleSearchPage.searchInputBox, "techglobal")
+                .keyUp(Keys.SHIFT)
+                .pause(Duration.ofSeconds(2))
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a")
+                .pause(Duration.ofSeconds(2))
+                .sendKeys("c")
+                .keyUp(Keys.CONTROL)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .pause(Duration.ofSeconds(2))
+                .keyDown(Keys.CONTROL).sendKeys("v")
+                .perform();
+
+        Waiter.pause(2);
+
+        Assert.assertEquals(googleSearchPage.searchInputBox.getAttribute("value"), "TECHGLOBALTECHGLOBAL");
+    }
+}
